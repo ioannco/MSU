7aa514f64d92df8234e952f6845a74989ad15ce3
foks210 <cheremisenov96@gmail.com>
2021-09-14 10:18:43 +0300

    added clion build files

---
+++ CHomework/cmake-build-debug/CMakeFiles/homework2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework2.dir/homework2.c.o"
+  "homework2"
+  "homework2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/homework2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
