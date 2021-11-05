9479ef9da447c9203a9f4e827c7ff23533bffa7a
Fokz210 <cheremisenov96@gmail.com>
2021-11-05 16:36:03 +0300

    ended task4

--- ejudge/task4.c
+++ ejudge/task4.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BUFFER_SIZE 16
+
 struct Node
 {
     struct Node *prev, *next;
@@ -13,16 +15,18 @@ struct List
     struct Node *first, *last;
 };
 
-#define STRING_COUNT 3
-
+char ** get_strings (unsigned int * strings_count);
+void print_list (struct List * pl);
 void process (struct List *pl, const char *str);
 
 int main ()
 {
-    char *strs[STRING_COUNT] = {"f", "e", "d"};
+    unsigned int strings_count = 0;
+    char ** strs = get_strings (&strings_count);
     struct List *pl = malloc (sizeof (struct Node));
     int i = 0;
     struct Node * current = NULL;
+    char control_str[16] = "";
 
     pl->first = (struct Node *) malloc (sizeof (struct Node));
     pl->first->prev = pl->first->next = NULL;
@@ -31,7 +35,7 @@ int main ()
     current->elem = (char *) malloc (strlen (strs[0]) + 1);
     strcpy (current->elem, strs[0]);
 
-    for (i = 1; i < STRING_COUNT; i++)
+    for (i = 1; i < strings_count; i++)
     {
         current->next = (struct Node *) malloc (sizeof (struct Node));
         current->next->prev = current;
@@ -45,42 +49,108 @@ int main ()
 
     pl->last = current;
 
+    printf ("enter control string: ");
+    scanf ("%s", control_str);
+
     printf ("list before process: \n");
 
-    for (current = pl->first; current != NULL; current = current->next)
-    {
-        printf("%s\n", current->elem);
-    }
+    print_list (pl);
     
-    process (pl, "b");
+    process (pl, control_str);
 
-    printf ("list after: \n");
+    printf ("\nlist after: \n");
 
-    for (current = pl->first; current != NULL; current = current->next)
+    print_list (pl);
+
+    for (current = pl->first; current != NULL;)
     {
-        printf("%s\n", current->elem);
+        struct Node * deletant = current;
+        current = current->next;
 
-        printf ("el->prev = %X, el->next = %X\n", current->prev, current->next);
+        free (deletant);
+        deletant = NULL;
     }
 
     free(pl);
+    pl = NULL;
+
+    for (i = 0; i < strings_count; i++)
+    {
+        free (strs[i]);
+        strs[i] = NULL;
+    }
+
+    free (strs);
+    strs = NULL;
+
+    return 0;
+}
+
+char ** get_strings (unsigned int * strings_count)
+{
+    unsigned int i = 0;
+    char ** strings = NULL;
+
+    printf ("enter strings count: ");
+    scanf ("%u", strings_count);
+
+    strings = (char **) malloc (sizeof (char *) * (*strings_count));
+
+    printf ("enter strings:\n\n");
+
+    for (; i < *strings_count; i++)
+    {
+        printf ("> ");
+
+        strings[i] = (char *) malloc (BUFFER_SIZE);
+        scanf ("%s", strings[i]);
+    }
+
+    printf ("\nstring input completed!\n");
+
+    return strings;
+}
+
+void print_list (struct List * pl)
+{
+    struct Node * current = pl->first;
+
+    for (current = pl->first; current != NULL; current = current->next)
+    {
+        printf("'%s'  ", current->elem);
+        printf ("(el = [0x%X], el->prev = [0x%X], el->next = [0x%X])\n", current, current->prev, current->next);
+    }
 }
 
 void process (struct List *pl, const char *str)
 {
-    if (!pl || !pl->first || !pl->last)
-        return;
-
     struct Node * current = pl->first;
     struct Node * end = pl->last;
+    struct Node * last_processed = NULL;
+
+    printf ("\n[process]: started processing. Control string is '%s'.\n", str);
+
+    if (!pl || !pl->first || !pl->last)
+    {
+        printf ("[process]: null pointer found.\n");
+        return;
+    }
 
     if (current == end)
+    {
+        printf ("[process]: current == end.\n");
         return;
+    }
 
     do
     {
+        printf ("[process]: current->elem = '%s', current = [0x%X], current->prev = [0x%X], current->next = [0x%X], end = [0x%X]\n", current->elem, current, current->prev, current->next, end);
+        last_processed = current;
+
         if (strcmp (current->elem, str) == 0)
         {
+            printf ("[process]: current node's string is equal to control string, deleting node...\n");
+
             if (current->prev)
                 current->prev->next = current->next;
 
@@ -104,6 +174,8 @@ void process (struct List *pl, const char *str)
         }
         else if (strcmp (current->elem, str) > 0)
         {
+            printf ("[process]: current node's string is larger than control string, moving node to the end of list...\n");
+
             if (!current->next)
                 break;
 
@@ -128,8 +200,17 @@ void process (struct List *pl, const char *str)
             current = next;
         }
         else
+        {
+            printf ("[process]: current node's string is smaller than control string, skipping node...\n");
             current = current->next;
+        }
 
-    } while (current && current != end);
+        printf ("[process]: list after iteration:\n");
+        print_list (pl);
+        printf ("[process]: iteration completed. current->prev = [0x%X], end = [0x%X]\n\n", current->prev, end);
+
+    } while (current && last_processed != end);
+
+    printf ("[process]: process completed\n");
 }
 
