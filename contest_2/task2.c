85ceeca83cb4347c870c4109f9498eb03b93ac9c
Fokz210 <cheremisenov96@gmail.com>
2021-12-20 02:46:55 +0300

    ended task2

--- contest_2/task2.c
+++ contest_2/task2.c
@@ -2,3 +2,61 @@
 // Created by Ivan on 12/13/2021.
 //
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int main(int argc, char ** argv)
+{
+    pid_t pid = -1;
+    int nums[20];
+    int pipes[2];
+    int fd1, fd2;
+
+    pipe (pipes);
+    pid = fork ();
+
+    if (pid)
+    {
+        fd1 = open (argv[1], O_RDONLY);
+        fd2 = open (argv[2], O_WRONLY | O_CREAT, 0777);
+
+        if (read (fd1, nums, sizeof (int) * 20) <= 0)
+        {
+            close (pipes[0]);
+            close (pipes[1]);
+            close (fd1);
+            close (fd2);
+            kill (pid, SIGINT);
+            wait (NULL);
+            return 0;
+        }
+        write (pipes[1], nums, sizeof(int) * 20);
+
+        while (!flag);
+
+        read (pipes[0], nums, sizeof (int) * 20);
+        write (fd2, nums, sizeof (int) * 20);
+
+
+    }
+    else
+    {
+        int i = 0;
+        if (read (pipes[0], nums, sizeof (int) * 20) <= 0)
+        {
+            close (pipes[0]);
+            close (pipes[1]);
+            return 0;
+        }
+
+        for (i = 0; i < 20; i++)
+            if (nums[i] % 2 == 1)
+                nums[i] = 0;
+
+        write(pipes[1], nums, sizeof (int) * 20);
+    }
+
+    return 0;
+}
